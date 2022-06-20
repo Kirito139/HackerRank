17ae56c51da5abaa65c4fc6b251750735d21c123
Lennier Mulder <99382674+Kirito139@users.noreply.github.com>
2022-06-20 14:08:16 -0700

    Works

--- 2D Arrays-DS.playground/Contents.swift
+++ 2D Arrays-DS.playground/Contents.swift
@@ -8,45 +8,48 @@ import Foundation
  * The function accepts 2D_INTEGER_ARRAY arr as parameter.
  */
 
-func hourglassSum(arr: [[Int]]) -> Int {
-    var x = [0, 1, 2]
-    var y = [0, 1, 2]
+func compute(x: Int , y: Int, arr: [[Int]]) -> Int {
     var currentSum = 0
+//    for i in 0 ... 2 {
+//        if i != 1 {
+//            for j in 0 ... 2 {
+//                 print(x, y, j, i)
+//                currentSum += arr[y+i][x+j]
+//                 print("add",arr[y[i]][x[j]])
+//            }
+//        } else {
+//            currentSum += arr[y+i][x+1]
+//             print("neck",arr[y[i]][x[1]])
+            
+//        }
+//    }
+    return arr[y][x] + arr[y][x+1] + arr[y][x+2] + arr[y+1][x+1] + arr[y+2][x] + arr[y+2][x+1] + arr[y+2][x+2]
+}
+
+func hourglassSum(arr: [[Int]]) -> Int {
+    var x = 0
+    var y = 0
     var greatestSum = 0
-    // Write your code here
-    for _ in 0 ... 15 {
-        for i in 0 ... 2 {
-            if i != 1 {
-                for j in 0 ... 2 {
-                // print(x, y, j, i)
-                currentSum += arr[y[i]][x[j]]
-                print("add",arr[y[i]][x[j]])
-                }
-            } else {
-                currentSum += arr[y[i]][x[1]]
-                print("neck",arr[y[i]][x[1]])
-            
-            }
-        }
-        if x[2] == 5 {
-            x = [0, 1, 2]
-            // if y[2] != 5
-            for i in 0 ... 2 {
-                y[i] += 1
-            }
+    var currentSum = 0
+//     Write your code here
+//     print(x,y)
+    for i in 0 ... 15 {
+        currentSum = compute(x: x, y: y, arr: arr)
+        if x == 3 {
+            x = 0
+            y += 1
         } else {
-            for i in 0 ... 2 {
-                x[i] += 1
-            }
-            
+            x += 1
+        }
+        if i == 0 {
+            greatestSum = currentSum
         }
         if currentSum > greatestSum {
             greatestSum = currentSum
         }
-        print(currentSum, greatestSum)
-        currentSum = 0
     }
-    print(greatestSum)
+    print(greatestSum,"is the greatestSum")
     return greatestSum
 }
-hourglassSum(arr: [[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 9, 2, -4, -4, 0], [0, 0, 0, -2, 0, 0], [0, 0, -1, -2, -4, 0]])
+//hourglassSum(arr: [[1, 1, 1, 0, 0, 0], [0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 9, 2, -4, -4, 0], [0, 0, 0, -2, 0, 0], [0, 0, -1, -2, -4, 0]])
+hourglassSum(arr: [[-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1], [-1, -1, -1, -1, -1, -1]])
