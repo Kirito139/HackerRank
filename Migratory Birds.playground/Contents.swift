f8efd3aa5d878ee4bced5229bb3539d2f0dc7ff8
Lennier Mulder <99382674+Kirito139@users.noreply.github.com>
2022-07-09 16:54:27 -0700

    Works

--- Migratory Birds.playground/Contents.swift
+++ Migratory Birds.playground/Contents.swift
@@ -9,15 +9,19 @@ import Foundation
 
 func migratoryBirds(arr: [Int]) -> Int {
     // Write your code here
-    var smallestFrequentBirds = 0
+    var smallestFrequentBirdID = 1
     var IDs = [0, 0, 0, 0, 0]
-    for i in arr {
-        IDs[i - 1] += 1
+    for id in arr {
+        IDs[id - 1] += 1
     }
-    for i in 1 ... IDs.endIndex {
-        if IDs[i - 1] > smallestFrequentBirds {
-            smallestFrequentBirds = i
+    for i in 1 ... 5 {
+        if IDs[i - 1] > IDs[smallestFrequentBirdID - 1] {
+            smallestFrequentBirdID = i
         }
     }
-    return smallestFrequentBirds
+    return smallestFrequentBirdID
 }
+
+assert(4 == migratoryBirds(arr: [1, 4, 4, 4, 5, 3]))
+assert(1 == migratoryBirds(arr: [4, 2, 1, 1, 3, 5, 4, 2, 3, 1]))
+assert(2 == migratoryBirds(arr: [5, 4, 2]))
